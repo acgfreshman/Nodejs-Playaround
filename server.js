36f774ae4ca0603d87e6d96bb298c185da2cb69c
acgfreshman <liumuchen1996@gmail.com>
2017-04-03 20:12:01 -0700

    Finish upload and preview function

--- server.js
+++ server.js
@@ -3,12 +3,9 @@ var url = require("url");
 
 function start(route, handle) {
   function onRequest(request, response) {
-      var pathname = url.parse(request.url).pathname;
-      console.log("Request for " + pathname + " received.");
-      response.writeHead(200, {"Content-Type": "text/plain"});
-      var content = route(handle,pathname);
-      response.write(content);
-      response.end();
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+    route(handle, pathname, response, request);
   }
 
   http.createServer(onRequest).listen(8888);
